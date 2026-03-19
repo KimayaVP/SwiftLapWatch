d285387dddea084c27a47197cc1429372fc41923
Kimaya <kimaya@vishalparwani.com>
2026-03-19 18:50:39 +0530

    Initial Xcode project for SwiftLapWatch

---
+++ SwiftLapWatch Watch App/SwiftLapWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLapWatchApp.swift
+//  SwiftLapWatch Watch App
+//
+//  Created by Kimaya on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLapWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
